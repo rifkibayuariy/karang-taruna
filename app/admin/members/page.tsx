67c2a051097e3104849759821960a31be9cec5c5
rifkibayuriy <email.rifkibayu@gmail.com>
2025-05-20 01:16:10 +0700

    padding search

--- app/admin/members/page.tsx
+++ app/admin/members/page.tsx
@@ -27,9 +27,9 @@ export default async function Members(props: {
                 <div className="md:flex-1/3 flex justify-end pt-8 md:pt-0">
                     <div className="relative w-full md:w-100">
                     <input
-                        className="block peer h-full w-full rounded-xl py-3 pl-10 text-sm bg-white shadow-sm placeholder:text-gray-500 focus:outline-none"
+                        className="block peer h-full w-full rounded-xl py-3 pl-12 text-sm bg-white shadow-sm placeholder:text-gray-500 focus:outline-none"
                     />
-                    <MagnifyingGlassIcon className="absolute left-3 top-1/2 size-5 -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                    <MagnifyingGlassIcon className="absolute left-4 top-1/2 size-5 -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
                     </div>
                 </div>
             </div>
