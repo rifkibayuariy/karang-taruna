78235c3a81baeb91bc2b1cd0237e753922f00452
rifkibayuriy <email.rifkibayu@gmail.com>
2025-05-22 21:59:22 +0700

    Change Layout Dashboard on mobile

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import { 
     MagnifyingGlassIcon,
     BanknotesIcon,
+    ArrowUpIcon,
+    ArrowDownIcon
 } from '@heroicons/react/24/outline';
 import { 
     ArrowUpCircleIcon,
-    ArrowDownCircleIcon
+    ArrowDownCircleIcon,
+    BanknotesIcon as BanknotesIconSolid
 } from '@heroicons/react/24/solid';
 
 export default function Dashboard() {
@@ -14,7 +17,7 @@ export default function Dashboard() {
                 <div className="hidden w-full lg:w-56 md:flex items-center">
                     <h1 className="text-xl text-nowrap md:text-2xl font-bold">Dashboard</h1>
                 </div>
-                <div className="w-full lg:w-74 xl:w-120 flex justify-end pt-8 md:pt-0">
+                <div className="w-full lg:w-74 xl:w-120 flex justify-end">
                     <div className="relative w-full">
                     <input
                         className="block peer z-0 h-full w-full rounded-xl py-3 pl-12 text-sm bg-white shadow-sm placeholder:text-gray-500 focus:outline-none"
@@ -24,18 +27,18 @@ export default function Dashboard() {
                     </div>
                 </div>
             </div>
-            <div className="mt-6 text-sm grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+            <div className="mt-6 text-sm grid grid-cols-2 md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
                 <div className="p-5 bg-white shadow-sm rounded-xl">
                     <div className="relative">
                         <span className="block">Income</span>
                         <div className="pt-3 font-bold">
                             <span className="text-sm">Rp. </span>
-                            <span className="text-xl">500.000</span>
+                            <span className="md:text-xl">500.000</span>
                         </div>
-                        <div className="absolute right-0 top-1/2 -translate-y-1/2">
-                            <BanknotesIcon className='size-10'/>
+                        <div className="absolute right-0 top-0 md:top-1/2 md:-translate-y-1/2">
+                            <BanknotesIcon className='size-6 md:size-10'/>
                             <span className="absolute bottom-0 right-0 p-0.25 rounded-full bg-white">
-                                <ArrowUpCircleIcon className='size-5 text-green-500'/>
+                                <ArrowUpCircleIcon className='size-3 md:size-5 text-green-500'/>
                             </span>
                         </div>
                     </div>
@@ -45,13 +48,40 @@ export default function Dashboard() {
                         <span className="block">Expense</span>
                         <div className="pt-3 font-bold">
                             <span className="text-sm">Rp. </span>
-                            <span className="text-xl">100.000</span>
+                            <span className="md:text-xl">100.000</span>
                         </div>
-                        <div className="absolute right-0 top-1/2 -translate-y-1/2">
-                            <BanknotesIcon className='size-10'/>
+                        <div className="absolute right-0 top-0 md:top-1/2 md:-translate-y-1/2">
+                            <BanknotesIcon className='size-6 md:size-10'/>
                             <span className="absolute bottom-0 right-0 p-0.25 rounded-full bg-white">
-                                <ArrowDownCircleIcon className='size-5 text-red-500'/>
+                                <ArrowDownCircleIcon className='size-3 md:size-5 text-red-500'/>
                             </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-5 bg-white shadow-sm rounded-xl">
+                    <div className="relative">
+                        <span className="block">Net Income</span>
+                        <div className="pt-3 font-bold">
+                            <span className="text-sm">Rp. </span>
+                            <span className="md:text-xl">400.000</span>
+                        </div>
+                        <div className="absolute right-0 top-0 md:top-1/2 md:-translate-y-1/2">
+                            <div className="flex">
+                                <ArrowUpIcon className='size-3 md:size-5 text-green-500'/>
+                                <ArrowDownIcon className='size-3 md:size-5 -ml-1 text-red-500'/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="p-5 bg-white shadow-sm rounded-xl">
+                    <div className="relative">
+                        <span className="block">Balance</span>
+                        <div className="pt-3 font-bold">
+                            <span className="text-sm">Rp. </span>
+                            <span className="md:text-xl">5.200.000</span>
+                        </div>
+                        <div className="absolute right-0 top-0 md:top-1/2 md:-translate-y-1/2">
+                            <BanknotesIconSolid className='size-6 md:size-10'/>
                         </div>
                     </div>
                 </div>
