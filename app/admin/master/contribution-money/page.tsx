ee405e9d1e032cac287790eca3b527cfe40ffb63
rifkibayuriy <email.rifkibayu@gmail.com>
2025-05-23 00:17:59 +0700

    Update Master Page

--- app/admin/master/contribution-money/page.tsx
+++ app/admin/master/contribution-money/page.tsx
@@ -9,32 +9,30 @@ import {
 
 export default async function ContributionMoney() {
     return (
-        <main className="pb-8">
-            <div className="w-full md:pt-4">
-                <div className="w-full flex flex-col gap-6">
-                    <div className="w-full md:order-2">
-                        <Breadcrumb/>
-                    </div>
-                    <h1 className="text-xl text-nowrap md:text-2xl font-bold text-center md:text-left">
-                        <span className="hidden md:inline-block mr-1">
-                            Master -</span>Contribution Money
-                    </h1>
+        <main className="pb-8 md:pt-8">
+            <div className="w-full flex flex-col gap-6 md:gap-3">
+                <div className="w-full md:order-2">
+                    <Breadcrumb/>
                 </div>
-                <div className="w-full md:max-w-148 rounded-xl bg-white shadow-sm p-5 mt-8">
-                    <div className="flex items-center">
-                        <BanknotesIcon className="size-6 mr-3"/>
-                        <span className="font-semibold">Current</span>
-                    </div>
-                    <div className="flex flex-col gap-3.5 justify-center items-center pt-6 pb-6">
-                        <span className="text-4xl font-bold">Rp. 5.000</span>
-                        <button className="px-4 py-2 bg-gray-800 rounded-xl text-white text-sm flex items-center justify-center gap-2">
-                            <PencilSquareIcon className="size-4"/>
-                            <span className="font-semibold">Change</span>
-                        </button>
-                    </div>
+                <h1 className="text-xl text-nowrap md:text-2xl font-bold text-center md:text-left">
+                    <span className="hidden md:inline-block mr-1">
+                        Master -</span>Contribution Money
+                </h1>
+            </div>
+            <div className="w-full md:max-w-148 rounded-xl bg-white shadow-sm p-5 mt-8">
+                <div className="flex items-center">
+                    <BanknotesIcon className="size-6 mr-3"/>
+                    <span className="font-semibold">Current</span>
+                </div>
+                <div className="flex flex-col gap-3.5 justify-center items-center pt-6 pb-6">
+                    <span className="text-4xl font-bold">Rp. 5.000</span>
+                    <button className="px-4 py-2 bg-gray-800 rounded-xl text-white text-sm flex items-center justify-center gap-2">
+                        <PencilSquareIcon className="size-4"/>
+                        <span className="font-semibold">Change</span>
+                    </button>
                 </div>
             </div>
-            <div className="pt-6 md:pt-8 md:max-w-148 ">
+            <div className="pt-4 md:pt-6 md:max-w-148 ">
                 <h4 className="font-bold pb-3">
                     <ClockIcon className="size-6 inline-block mr-3"/>
                     History
--- app/admin/master/location/page.tsx
+++ app/admin/master/location/page.tsx
@@ -1,33 +1,38 @@
+import Breadcrumb from "@/components/admin/breadcrumb";
 import Table from "@/components/admin/master/location/table";
 
 import { MagnifyingGlassIcon, PlusIcon } from '@heroicons/react/24/solid';
 
 export default async function Location() {
     return (
-        <main className="pb-8">
-            <div className="flex flex-col md:flex-row w-full gap-2 justify-between items-center md:pt-4">
-                <div className="flex items-center">
-                    <h1 className="text-xl text-nowrap md:text-2xl font-bold"><span className="hidden md:inline-block mr-1">Master -</span>Location</h1>
+        <main className="pb-10 md:pt-8">
+            <div className="w-full flex flex-col gap-6 md:gap-3 pb-8 md:pb-10">
+                <div className="w-full md:order-2">
+                    <Breadcrumb/>
                 </div>
-                <div className="flex flex-row gap-2 w-full pt-4 md:pt-0">
-                    <div className="flex flex-auto justify-center md:items-center">
-                        <button className="px-3 py-2 bg-gray-800 rounded-xl text-white text-sm flex items-center justify-center gap-2">
-                            <PlusIcon className="size-4"/>
-                            <span className="font-semibold">New <span className="hidden md:inline-block">Location</span></span>
-                        </button>
-                    </div>
-                    <div className="w-full md:w-100 flex justify-end">
-                        <div className="relative w-full">
-                        <input
-                            className="block peer z-0 h-full w-full rounded-xl py-3 pl-12 text-sm bg-white shadow-sm placeholder:text-gray-500 focus:outline-none"
-                            placeholder="Search"
-                        />
-                        <MagnifyingGlassIcon className="absolute left-4 top-1/2 size-5 -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-                        </div>
+                <h1 className="text-xl text-nowrap md:text-2xl font-bold text-center md:text-left">
+                    <span className="hidden md:inline-block mr-1">
+                        Master -</span>Location
+                </h1>
+            </div>
+            <div className="flex flex-row gap-2 w-full">
+                <div className="flex flex-auto md:items-center">
+                    <button className="px-3 py-2 bg-gray-800 rounded-xl text-white text-sm flex items-center justify-center gap-2">
+                        <PlusIcon className="size-4"/>
+                        <span className="font-semibold">New <span className="hidden lg:inline-block">Location</span></span>
+                    </button>
+                </div>
+                <div className="w-full md:w-100 flex justify-end">
+                    <div className="relative w-full">
+                    <input
+                        className="block peer z-0 h-full w-full rounded-xl py-3 pl-12 text-sm bg-white shadow-sm placeholder:text-gray-500 focus:outline-none"
+                        placeholder="Search"
+                    />
+                    <MagnifyingGlassIcon className="absolute left-4 top-1/2 size-5 -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
                     </div>
                 </div>
             </div>
-            <div className="pt-4 md:pt-8">
+            <div className="pt-4 md:pt-6">
                 <Table/>
             </div>
         </main>
