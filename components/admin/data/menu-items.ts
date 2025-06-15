cbdc5295156b61e423bca3377573f4fb5dfdc1dc
rifkibayuariy <email.rifkibayu@gmail.com>
2025-06-15 15:56:45 +0700

    Change Icon SideNav

--- components/admin/data/menu-items.ts
+++ components/admin/data/menu-items.ts
@@ -1,20 +1,30 @@
 import {
-  HomeIcon,
-  UsersIcon,
-  BookOpenIcon,
   BanknotesIcon,
   MapPinIcon,
-  CurrencyDollarIcon,
   HandRaisedIcon,
 } from "@heroicons/react/24/outline";
+import { Home, Users, SquareLibrary, CircleDollarSign } from "lucide-react";
 
 export const menuItems = [
-  { name: 'Dashboard', href: '/admin/dashboard', icon: HomeIcon },
-  { name: 'Members', href: '/admin/members', icon: UsersIcon },
-  { name: 'Monthly Meeting', href: '/admin/monthly-meeting', icon: HandRaisedIcon },
-  { name: 'Cash', href: '/admin/cash', icon: BanknotesIcon },
-  { name: 'Master', href: '/admin/master', icon: BookOpenIcon, child:[
-    { name: 'Location', href: '/admin/master/location', icon: MapPinIcon },
-    { name: 'Contribution Money', href: '/admin/master/contribution-money', icon: CurrencyDollarIcon },
-  ]}
+  { name: "Dashboard", href: "/admin/dashboard", icon: Home },
+  { name: "Members", href: "/admin/members", icon: Users },
+  {
+    name: "Monthly Meeting",
+    href: "/admin/monthly-meeting",
+    icon: HandRaisedIcon,
+  },
+  { name: "Cash", href: "/admin/cash", icon: BanknotesIcon },
+  {
+    name: "Master",
+    href: "/admin/master",
+    icon: SquareLibrary,
+    child: [
+      { name: "Location", href: "/admin/master/location", icon: MapPinIcon },
+      {
+        name: "Contribution Money",
+        href: "/admin/master/contribution-money",
+        icon: CircleDollarSign,
+      },
+    ],
+  },
 ];
