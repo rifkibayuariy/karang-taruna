e40f0c36ff056865b8311db9df3fade3660c1629
rifkibayuariy <email.rifkibayu@gmail.com>
2025-06-16 16:46:07 +0700

    Dialog Delete Location

--- components/admin/master/location/columns.tsx
+++ components/admin/master/location/columns.tsx
@@ -2,8 +2,19 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { Button } from "@/components/admin/ui/button";
-import { SquarePen, Trash2 } from "lucide-react";
+import { SquarePen, CircleAlert, CircleX, Trash2 } from "lucide-react";
 import FormLocationDialog from "@/components/admin/master/location/form-dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/admin/ui/alert-dialog";
 
 export type Location = {
   id: number;
@@ -33,14 +44,47 @@ export const columns: ColumnDef<Location>[] = [
             <span className="sr-only">Update</span>
           </Button>
         </FormLocationDialog>
-        <Button
-          size="sm"
-          className="bg-red-400 hover:bg-red-500 cursor-pointer"
-          onClick={() => alert(row.original.id)}
-        >
-          <Trash2 className="size-4" />
-          <span className="sr-only">Delete</span>
-        </Button>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button
+              size="sm"
+              className="bg-red-400 hover:bg-red-500 cursor-pointer"
+            >
+              <Trash2 className="size-4" />
+              <span className="sr-only">Delete</span>
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader className="mb-4">
+              <div className="flex justify-center">
+                <CircleAlert className="size-14 bg-red-400 p-2 text-white rounded-full" />
+              </div>
+              <AlertDialogTitle className="text-center text-red-500">
+                Delete Location?
+              </AlertDialogTitle>
+              <AlertDialogDescription className="text-center text-zinc-700">
+                This action cannot be undone. This action will delete the
+                location. Delete
+                <span className="font-extrabold mx-1.5">
+                  {row.original.name}
+                </span>
+                location?
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter className="md:justify-center">
+              <AlertDialogCancel>
+                <CircleX className="size-4" />
+                <span className="font-semibold text-zinc-700">Cancel</span>
+              </AlertDialogCancel>
+              <AlertDialogAction asChild>
+                <button className="bg-red-400 hover:bg-red-500">
+                  <Trash2 className="size-4" />
+                  <span className="font-semibold">Delete</span>
+                </button>
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </div>
     ),
   },
