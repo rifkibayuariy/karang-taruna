80c495213f8071ded9b662f65e4cbe93c0be426f
Jati Sri Pamungkas <jatispamungkas357@gmail.com>
2025-07-08 07:14:52 +0700

    Diisabled menu  Location Detail

--- data/navigation.ts
+++ data/navigation.ts
@@ -1,57 +1,57 @@
 import { BanknotesIcon, HandRaisedIcon } from "@heroicons/react/24/outline";
 import {
-  Home,
-  Users,
-  SquareLibrary,
-  CircleDollarSign,
-  IdCardLanyard,
-  LayoutList,
-  MapPin,
-  MapPinned,
+	Home,
+	Users,
+	SquareLibrary,
+	CircleDollarSign,
+	IdCardLanyard,
+	LayoutList,
+	MapPin,
+	// MapPinned,
 } from "lucide-react";
 
 export interface NavItem {
-  name: string;
-  href: string;
-  icon: React.ElementType;
-  child?: NavItem[];
+	name: string;
+	href: string;
+	icon: React.ElementType;
+	child?: NavItem[];
 }
 
 export const menuItems: NavItem[] = [
-  { name: "Dashboard", href: "/admin/dashboard", icon: Home },
-  { name: "Members", href: "/admin/members", icon: Users },
-  {
-    name: "Monthly Meeting",
-    href: "/admin/monthly-meeting",
-    icon: HandRaisedIcon,
-  },
-  { name: "Cash", href: "/admin/cash", icon: BanknotesIcon },
-  {
-    name: "Master",
-    href: "/admin/master",
-    icon: SquareLibrary,
-    child: [
-      { name: "Location", href: "/admin/master/location", icon: MapPin },
-      {
-        name: "Location Detail",
-        href: "/admin/master/location-detail",
-        icon: MapPinned,
-      },
-      {
-        name: "Contribution Money",
-        href: "/admin/master/contribution-money",
-        icon: CircleDollarSign,
-      },
-      {
-        name: "Org Positions",
-        href: "/admin/master/org-positions",
-        icon: IdCardLanyard,
-      },
-      {
-        name: "Org Structure",
-        href: "/admin/master/org-structure",
-        icon: LayoutList,
-      },
-    ],
-  },
+	{ name: "Dashboard", href: "/admin/dashboard", icon: Home },
+	{ name: "Members", href: "/admin/members", icon: Users },
+	{
+		name: "Monthly Meeting",
+		href: "/admin/monthly-meeting",
+		icon: HandRaisedIcon,
+	},
+	{ name: "Cash", href: "/admin/cash", icon: BanknotesIcon },
+	{
+		name: "Master",
+		href: "/admin/master",
+		icon: SquareLibrary,
+		child: [
+			{ name: "Location", href: "/admin/master/location", icon: MapPin },
+			// {
+			//   name: "Location Detail",
+			//   href: "/admin/master/location-detail",
+			//   icon: MapPinned,
+			// },
+			{
+				name: "Contribution Money",
+				href: "/admin/master/contribution-money",
+				icon: CircleDollarSign,
+			},
+			{
+				name: "Org Positions",
+				href: "/admin/master/org-positions",
+				icon: IdCardLanyard,
+			},
+			{
+				name: "Org Structure",
+				href: "/admin/master/org-structure",
+				icon: LayoutList,
+			},
+		],
+	},
 ];
